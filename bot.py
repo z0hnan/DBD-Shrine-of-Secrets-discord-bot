da20718db2f178312fd1796c52806723ed24f37c
z0hnan <simesim2006@gmail.com>
2023-01-10 17:29:24 +0100

    new token oops haha

--- bot.py
+++ bot.py
@@ -25,4 +25,4 @@ async def on_ready():
         
         await asyncio.sleep(3600)
 
-client.run('MTA2MjA2Mjc4NTQwNTY1MzAzMg.Gw541F.DfeFJw-pDxAgd6gVbYGHGLYDtYQxGfSyiwCI2s')
+client.run('MTA2MjA2Mjc4NTQwNTY1MzAzMg.GG7uBD.7srWcTHApNPXmgTLFwQlvE3N6fX9ga7Uvmw7NQ')
